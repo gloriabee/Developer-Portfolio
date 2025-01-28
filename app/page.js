8e88ebf8f426ccc1cc39114f7b8d1fdf3c9660c2
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-28 23:46:24 +0700

    Project setup

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+      <div className='max-w-7xl w-full'>
+        <h1>Hello, Portfolio!</h1>
+      </div>
+    </main>
   )
 }
 
