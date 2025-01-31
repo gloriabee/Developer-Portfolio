24a4f6fc740444d83146f5d71ce70d99c3372ae6
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-31 11:17:20 +0700

    dynamic routes

---
+++ app/userslist/[user]/page.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+const User = ({ params }) => {
+    console.log(params);
+    return (
+        <div>
+            <h1>Info about {params.user}</h1>
+            <p>Hello I'm {params.user}</p>
+        </div>
+    )
+}
+
+export default User
