ced3316ba5db26ea91ac5c54c6103566478a209b
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-02-01 23:02:37 +0700

    Not Found page

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='text-center text-4xl'>
+            404 NotFound
+        </div>
+    )
+}
+
+export default NotFound
